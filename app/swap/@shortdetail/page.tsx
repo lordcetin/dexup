c7645399d369b69a47c6b7fa71ffbcd0eee2aa19
lordcetin <yunuscetin3767150@gmail.com>
2024-05-14 21:25:58 +0300

    afgadgad

--- app/swap/@shortdetail/page.tsx
+++ app/swap/@shortdetail/page.tsx
@@ -118,7 +118,7 @@ const ShortDetail = ({}: Props) => {
   <div className="flex items-center w-full max-md:w-screen rounded-xl border border-white/10 bg-[#131722] mb-2 gap-x-2 px-7 py-5">
     <div className="flex items-center gap-x-6 max-md:grid max-md:grid-cols-2 max-md:gap-y-2">
         
-      <div className="flex items-center gap-x-6 relative cursor-pointer" onClick={() => setOpenModal(!openModal)}>
+      <div className="flex items-center gap-x-6 relative cursor-pointer max-md:col-span-full" onClick={() => setOpenModal(!openModal)}>
         <img src={tokenInfo?.data?.attributes?.image_url === 'missing.png' ? '/assets/missing.png' : tokenInfo?.data?.attributes?.image_url} alt={tokenInfo?.data?.attributes?.name} className="w-12 object-cover border border-white/10 rounded-full"/>
         <div className="absolute bottom-3 left-6"><DexImage dex={pairdata?.dex} size={8}/></div>
         <div className="flex-col items-center mt-3">
@@ -140,7 +140,7 @@ const ShortDetail = ({}: Props) => {
         : null}
       </div>
 
-      <div className="hidden max-md:flex"></div>
+      <div className="hidden max-md:flex gap-x-12 w-full mb-5">
       <div className="h-20 w-[2px] bg-gradient-to-b to-translate via-slate-600 from-transparent rounded-full max-md:hidden"></div>
         <div className="flex items-center gap-x-3">
           <div className="flex-col items-center mt-3">
@@ -164,16 +164,42 @@ const ShortDetail = ({}: Props) => {
           </Tippy>
           </div>
         </div>
+      </div>
+
       <div className="h-20 w-[2px] bg-gradient-to-b to-translate via-slate-600 from-transparent rounded-full max-md:hidden"></div>
-      <div className="flex-col flex justify-center text-center items-center relative">
-          <div className="flex items-center gap-x-1 justify-center text-center py-1 px-2 w-full text-[10px] max-md:text-[8px] absolute top-0 max-md:-left-14 -left-0">
+        <div className="flex items-center gap-x-3 max-md:hidden">
+          <div className="flex-col items-center mt-3">
+          <h1 className="text-xl flex gap-x-2">M. Cap</h1>
+          <div className="flex items-center gap-x-2 font-bold text-xl p-2 border border-white/10 rounded-lg mt-2"><h1 className="">{pairdata?.fdv === null ? '<$1' : pairdata?.fdv === 0 ? '<$1' : getAmount(pairdata?.fdv)}</h1></div>
+          </div>
+        </div>
+      <div className="h-20 w-[2px] bg-gradient-to-b to-translate via-slate-600 from-transparent rounded-full max-md:hidden"></div>
+        <div className="flex items-center gap-x-3 max-md:hidden">
+          <div className="flex-col items-center mt-3">
+          <h1 className="text-xl flex gap-x-2">Liquidity</h1>
+          <div className="flex items-center gap-x-2 font-bold text-xl p-2 border border-white/10 rounded-lg mt-2"><h1 className="">{getAmount(pairdata?.reserve)}</h1></div>
+          </div>
+        </div>
+      <div className="h-20 w-[2px] bg-gradient-to-b to-translate via-slate-600 from-transparent rounded-full max-md:hidden"></div>
+        <div className="flex items-center gap-x-3 max-md:hidden">
+          <div className="flex-col items-center mt-3">
+          <h1 className="text-xl flex gap-x-2">Volume</h1>
+          <Tippy content={`H24`}>
+          <div className="flex items-center gap-x-2 font-bold text-xl p-2 border border-white/10 rounded-lg mt-2"><h1 className="">{getAmount(pairdata?.volume_usd?.h24)}</h1></div>
+          </Tippy>
+          </div>
+        </div>
+
+      <div className="h-20 w-[2px] bg-gradient-to-b to-translate via-slate-600 from-transparent rounded-full max-md:hidden"></div>
+      <div className="flex-col flex justify-center text-center items-center relative max-md:col-span-full max-md:left-12">
+          <div className="flex items-center gap-x-1 justify-center text-center py-1 px-2 w-full text-[10px] max-md:text-xs absolute top-0 max-md:-left-14 -left-0">
             <button className={transtime === '30' ? "border bg-zinc-900 rounded-md px-3" : "px-3 hover:border hover:bg-zinc-900 rounded-md"} type="button" onClick={() => setTransacitonTime('30')}>30 minute</button>
             <button className={transtime === '15' ? "border bg-zinc-900 rounded-md px-3" : "px-3 hover:border hover:bg-zinc-900 rounded-md"} type="button" onClick={() => setTransacitonTime('15')}>15 minute</button>
             <button className={transtime === '5' ? "border bg-zinc-900 rounded-md px-3" : "px-3 hover:border hover:bg-zinc-900 rounded-md"} type="button" onClick={() => setTransacitonTime('5')}>5 minute</button>
             <button className={transtime === '1' ? "border bg-zinc-900 rounded-md px-3" : "px-3 hover:border hover:bg-zinc-900 rounded-md"} type="button" onClick={() => setTransacitonTime('1')}>1 hour</button>
             <button className={transtime === '24' ? "border bg-zinc-900 rounded-md px-3" : "px-3 hover:border hover:bg-zinc-900 rounded-md"} type="button" onClick={() => setTransacitonTime('24')}>24 hour</button>
           </div>
-          <div className="flex-col flex text-xs justify-center items-center w-full mt-12">
+          <div className="flex-col flex text-xs justify-center items-center relative max-md:-left-12 mt-12">
             <CustomProgressBar title1="Buys" title2="Sells"
             buys={transtime === '30' ? pairdata?.transactions?.m30?.buys : transtime === '15' ? pairdata?.transactions?.m15?.buys : transtime === '5' ? pairdata?.transactions?.m5?.buys : transtime === '1' ? pairdata?.transactions?.h1?.buys : transtime === '24' ? pairdata?.transactions?.h24?.buys : 0}
             sells={transtime === '30' ? pairdata?.transactions?.m30?.sells : transtime === '15' ? pairdata?.transactions?.m15?.sells : transtime === '5' ? pairdata?.transactions?.m5?.sells : transtime === '1' ? pairdata?.transactions?.h1?.sells : transtime === '24' ? pairdata?.transactions?.h24?.sells : 0} />
@@ -192,10 +218,17 @@ const ShortDetail = ({}: Props) => {
       </div>
       <div className="h-20 w-[2px] bg-gradient-to-b to-translate via-slate-600 from-transparent rounded-full max-md:hidden"></div>
       <div className="flex items-center gap-x-3">
-        <div className="flex-col items-center ">
+        <div className="flex-col items-center max-md:flex">
         <TokenScan chain={chain} address={pairdata?.baseaddress}/>
+          <div className="max-md:flex hidden mt-5 gap-x-4">
+            <FaRegHeart className="flex justify-end items-center text-white/30 hover:text-white transition-all size-6 cursor-pointer"/>
+            {/* <FaHeart className="flex justify-end items-center text-white/30 hover:text-white transition-all size-6 cursor-pointer" /> */}
+            <FaRegBell className="flex justify-end items-center text-white/30 hover:text-white transition-all size-6 cursor-pointer"/>
+            {/* <FaBell className="flex justify-end items-center text-white/30 hover:text-white transition-all size-6 cursor-pointer" /> */}
+            <IoMdShare className="flex justify-end items-center text-white/30 hover:text-white transition-all size-6 cursor-pointer"/>
+          </div>
         </div>
-        <div className="flex-col gap-y-4 flex relative left-5">
+        <div className="flex-col gap-y-4 flex relative max-md:hidden">
           <FaRegHeart className="flex justify-end items-center text-white/30 hover:text-white transition-all size-6 cursor-pointer"/>
           {/* <FaHeart className="flex justify-end items-center text-white/30 hover:text-white transition-all size-6 cursor-pointer" /> */}
           <FaRegBell className="flex justify-end items-center text-white/30 hover:text-white transition-all size-6 cursor-pointer"/>
--- components/CustomProgressBar/page.tsx
+++ components/CustomProgressBar/page.tsx
@@ -12,12 +12,12 @@ const CustomProgressBar: React.FC<CustomProgressBarProps> = ({ buys, sells,title
   const sellsWidth = (sells / total) * 100;
 
   return (
-    <div style={{ borderRadius: '5px',marginTop:'5px',marginBottom:'5px' }} className="w-72 block max-md:inline-block max-md:w-36">
-      <div className="flex max-md:w-36">
+    <div style={{ borderRadius: '5px',marginTop:'5px',marginBottom:'5px' }} className="w-72 block max-md:inline-block max-md:w-80">
+      <div className="flex max-md:w-80">
       <div title={`${title1+ " " +String(buysWidth)}`} style={{ width: `${buysWidth}%`, backgroundColor: '#22c55e', height: '8px', borderRadius: '5px 0 0 5px' }}></div>
       <div title={`${title2+ " " +String(sellsWidth)}`} style={{ width: `${sellsWidth}%`, backgroundColor: '#dc2626', height: '8px', borderRadius: '0 5px 5px 0' }}></div>
       </div>
-      <div className="flex justify-between items-center w-full max-md:w-36">
+      <div className="flex justify-between items-center w-full max-md:w-80">
         <div className="flex items-center gap-x-1 justify-start"><h1>{title1}</h1><span>{buys}</span></div>
         <div className="flex items-center gap-x-1 justify-end"><h1>{title2}</h1><span>{sells}</span></div>
       </div>
