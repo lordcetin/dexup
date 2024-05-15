a21141e4d2a69db67a335411b1160444c7bc841c
lordcetin <yunuscetin3767150@gmail.com>
2024-05-15 10:57:57 +0300

    adgdsags

--- middleware.ts
+++ middleware.ts
@@ -4,18 +4,18 @@ export function middleware(request: NextRequest){
   //@ts-ignore
   const cookieStore = cookies()
 
-  // const data:any = JSON.parse(cookieStore?.get('wagmi.store')?.value as any)
+  const data:any = JSON.parse(cookieStore?.get('wagmi.store')?.value as any)
 
-  // if(request.nextUrl.pathname === '/dashboard' && data?.state?.current === undefined){
-  //   return NextResponse.redirect(new URL('/', request?.url))
-  // }
+  if(request.nextUrl.pathname === '/dashboard' && data?.state?.current === undefined){
+    return NextResponse.redirect(new URL('/', request?.url))
+  }
 
-  // let walletAddress = data?.state?.connections?.value[0]?.map((item:any) => item?.accounts)?.[1]
-  // walletAddress = walletAddress[0]
+  let walletAddress = data?.state?.connections?.value[0]?.map((item:any) => item?.accounts)?.[1]
+  walletAddress = walletAddress[0]
 
-  // if(request.nextUrl.pathname === '/dashboard' && walletAddress !== '0x24999A8188a103a742828a933212F03e349e18C5'){
-  //   return NextResponse.redirect(new URL('/', request?.url))
-  // }
+  if(request.nextUrl.pathname === '/dashboard' && walletAddress !== '0x24999A8188a103a742828a933212F03e349e18C5'){
+    return NextResponse.redirect(new URL('/', request?.url))
+  }
 
   if(request.nextUrl.pathname === '/api'){
     return NextResponse.redirect(new URL('/', request?.url))
